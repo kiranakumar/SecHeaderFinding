eae0418c8f58e73c83b19598f4528559c1d425a6
Kiran A Kumar <kiranakumar333@gmail.com>
2024-09-26 09:37:12 +0530

    Update security_headers.py

--- security_headers.py
+++ security_headers.py
@@ -1,7 +1,6 @@
 import requests
 import sys
 
-# List of common security-related headers
 security_headers = [
     'Content-Security-Policy', 
     'Strict-Transport-Security', 
@@ -16,12 +15,11 @@ security_headers = [
 
 def get_security_headers(url):
     try:
-        # Send a HEAD request to the URL
+       
         response = requests.head(url, timeout=10)
         
         headers = response.headers
         
-        # Filter and display only security headers
         found_headers = {header: headers.get(header) for header in security_headers if header in headers}
         
         if found_headers:
@@ -34,11 +32,9 @@ def get_security_headers(url):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
-# Check if URL is passed as a command-line argument
 if len(sys.argv) != 2:
     print("Usage: python security_headers.py <URL>")
     sys.exit(1)
 
-# Get the URL from command-line argument
 url = sys.argv[1]
 get_security_headers(url)
